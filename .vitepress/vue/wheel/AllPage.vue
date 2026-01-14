799ed28c5dbdc5451cfd3ab0cedb3cb605a00739
Dahesor <dahesor@gmail.com>
2026-01-14 15:53:35 -0500

    🖼️更换全部页面图标以匹配

--- .vitepress/vue/wheel/AllPage.vue
+++ .vitepress/vue/wheel/AllPage.vue
@@ -2,7 +2,7 @@
 	<div class="page-frame">
 		<div class="search-container">
 			<div class="header">
-				<img src="/icons/bg4.png" alt="Logo" class="logo" />
+				<img src="/icons/cart_with_chest.png" alt="Logo" class="logo" />
 				<div class="title-container">
 					<span class="title">香草前置馆 — 全部资源</span>
 					<span class="subtitle">随意挑选自己最香草的前置</span>
